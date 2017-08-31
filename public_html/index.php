04effc2ed7575ff3c3b814140572b6fc0180752e
Dannielle Bojorquez <dbojorquez@cnm.edu>
2017-08-31 10:20:57 -0600

    added jquery validate form

--- public_html/index.php
+++ public_html/index.php
@@ -20,6 +20,10 @@
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+
 		<!-- Google reCAPTCHA -->
 		<script src='https://www.google.com/recaptcha/api.js'></script>
 
@@ -42,7 +46,7 @@
 					<div class="col-sm-6">
 
 						<!-- BEGIN CONTACT FORM -->
-						<form id="contact-form" action="" method="post" novalidate>
+						<form id="contact-form" action="php/mailer.php" method="post" novalidate>
 							<div class="form-group">
 								<label for="name">Name</label>
 								<div class="input-group">
